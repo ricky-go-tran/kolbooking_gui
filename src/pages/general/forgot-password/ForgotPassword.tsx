ec6dbd41fecda7333a3b12bf991fab2276a814b0
Ricky <ricky.tran.goldenowl@gmail.com>
2023-09-12 08:54:31 +0700

    Add todo forgotpassword

--- src/pages/general/forgot-password/ForgotPassword.tsx
+++ src/pages/general/forgot-password/ForgotPassword.tsx
@@ -5,6 +5,7 @@ import ImageDark from '../../../assets/images/forgot-password-office-dark.jpeg'
 import { Label, Input, Button } from '@windmill/react-ui'
 
 function ForgotPassword() {
+  //TODO: add this feature after add forgot_password routes and send email in backend
   return (
     <div className="flex items-center min-h-screen p-6 bg-gray-50 dark:bg-gray-900">
       <div className="flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800">
