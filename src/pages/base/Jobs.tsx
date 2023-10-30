5114bc8b515d2018ecfa1889b73cfcee00ab48e5
Ricky <ricky.tran.goldenowl@gmail.com>
2023-10-30 09:53:57 +0700

    Fix cancel base job

--- src/pages/base/Jobs.tsx
+++ src/pages/base/Jobs.tsx
@@ -50,7 +50,7 @@ const Jobs = () => {
 
   function cancle(job: ITableJob) {
     axios
-      .delete(getProxy(`/api/v1/base/jobs/${job.id}/cancle`), {
+      .put(getProxy(`/api/v1/base/jobs/${job.id}/cancle`), {
         headers: {
           Authorization: auth_state.auth_token,
         },
